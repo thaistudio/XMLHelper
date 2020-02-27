77f9c53e0569cf292a36346e8630237d6e7965e8
Thai Son Nguyen <ts.nguyen@strucsoftsolutions.com>
2020-02-27 08:26:36 -0500

    Last commit from Strucsoft

---
+++ XMLHelper/XMLHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLHelper
+{
+    public class XMLHelper
+    {
+        public static XmlNode AddNode(XmlDocument xmlDoc, string newNodeName,
+           string innerText = null,
+           string attributeName = null, string attributeValue = null,
+           XmlNode parentNode = null)
+        {
+            XmlNode newNode = xmlDoc.CreateElement(newNodeName);
+
+            // Add innertext
+            if (innerText != null)
+            {
+                newNode.InnerText = innerText;
+            }
+
+            // Add if attribute if there is any
+            if (attributeName != null)
+            {
+                XmlAttribute newNodeAttribute = xmlDoc.CreateAttribute(attributeName);
+                if (attributeValue != null) newNodeAttribute.Value = attributeValue;
+                newNode.Attributes.Append(newNodeAttribute);
+            }
+
+            if (parentNode != null) // Append newNode to parentNode
+            {
+                parentNode.AppendChild(newNode);
+            }
+            else // Append newNode to xmlDoc
+            {
+                xmlDoc.AppendChild(newNode);
+            }
+
+            return newNode;
+        }
+
+        public static void AddAttribute(XmlDocument xmlDoc, string attributeName, string attributeVal, XmlNode node)
+        {
+            XmlAttribute xmlAttribute = xmlDoc.CreateAttribute(attributeName);
+            xmlAttribute.Value = attributeVal;
+            node.Attributes.Append(xmlAttribute);
+        }
+    }
+}
